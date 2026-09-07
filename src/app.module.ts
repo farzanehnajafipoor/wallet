ee07ca4b6f89b344b6c120136868c26c5be66b0f
farzanehnajafipoor <fn.najafipoor@gmail.com>
2026-09-07 12:31:18 +0330

    chore: configure PostgreSQL with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,34 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [
-    // Distributed tracing, auto-correlated logs, request/job metrics, error
-    // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
+      ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  }),
+   TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+   useFactory: (configService: ConfigService) => {
+  return {
+    type: 'postgres',
+    host: configService.get<string>('DB_HOST'),
+    port: Number(configService.get<string>('DB_PORT')),
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_NAME'),
+    autoLoadEntities: true,
+    synchronize: true,
+  };
+},
+  }),
     ObserveModule.forRoot({
       appKey: 'YOUR_APP_KEY',
       appSecret: 'YOUR_APP_SECRET',
